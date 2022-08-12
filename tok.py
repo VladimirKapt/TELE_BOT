10e2b216fd0f37c69c4656dabc54d4d88ce49cc7
VladimirKapt <100538636+VladimirKapt@users.noreply.github.com>
2022-08-12 18:26:50 +0300

    Update tok.py

--- tok.py
+++ tok.py
@@ -1,4 +1,4 @@
 from envparse import env
 
-TELEGRAM_TOKEN = env.str("TELEGRAM_TOKEN", default="5491946752:AAGOItn3iXp54NXKdrCXJJZ4IGGKvi40fkU")
+TELEGRAM_TOKEN = env.str("TELEGRAM_TOKEN", default="TELEGRAM_TOKEN_ID")
 TEST_GR0UP_ID = env.int("TELEGRAM_TOKEN", default="-1001633628953")
